9a86f5583474777830b5709ed51d7cf7f978ae2c
Florian Fischer <florian.fischer@muhq.space>
2024-01-12 14:43:54 +0100

    init: restart the login process after the user session terminated

--- programs/init.c
+++ programs/init.c
@@ -1,24 +1,29 @@
 /// @file init.c
 /// @brief `init` program.
-/// @copyright (c) 2014-2023 This file is distributed under the MIT License.
+/// @copyright (c) 2014-2024 This file is distributed under the MIT License.
 /// See LICENSE.md for details.
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/unistd.h>
 #include <sys/wait.h>
-#include <time.h>
 
 int main(int argc, char *argv[], char *envp[])
 {
     char *_argv[] = { "login", NULL };
     int status;
-    if (fork() == 0) {
-        execv("/bin/login", _argv);
-        printf("This is bad, I should not be here! EXEC NOT WORKING\n");
-    }
+
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "EndlessLoop"
     while (1) {
-        wait(&status);
+        pid_t login = fork();
+        if (login == 0) {
+            execv("/bin/login", _argv);
+            printf("This is bad, I should not be here! EXEC NOT WORKING\n");
+        }
+
+        while (wait(&status) != login);
     }
+#pragma clang diagnostic pop
     return 0;
 }
