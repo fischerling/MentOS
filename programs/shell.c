f1b6b8c4681cbf150a740628ee2d0bdf6e27c1c9
Florian Fischer <florian.fischer@muhq.space>
2024-03-29 13:31:39 +0100

    shell: use the new libc readline function

--- programs/shell.c
+++ programs/shell.c
@@ -10,6 +10,7 @@
 #include <io/debug.h>
 #include <libgen.h>
 #include <limits.h>
+#include <readline.h>
 #include <signal.h>
 #include <stdbool.h>
 #include <stddef.h>
@@ -24,28 +25,11 @@
 #include <sys/wait.h>
 #include <termios.h>
 
-/// Maximum length of commands.
-#define CMD_LEN 64
-/// Maximum lenght of the history.
-#define HISTORY_MAX 10
-
 // Required by `export`
 #define ENV_NORM 1
 #define ENV_BRAK 2
 #define ENV_PROT 3
 
-// The input command.
-static char cmd[CMD_LEN] = { 0 };
-// The index of the cursor.
-static size_t cmd_cursor_index = 0;
-// History of commands.
-static char history[HISTORY_MAX][CMD_LEN] = { 0 };
-// The current write index inside the history.
-static int history_write_index = 0;
-// The current read index inside the history.
-static int history_read_index = 0;
-// Boolean used to check if the history is full.
-static bool_t history_full = false;
 // Store the last command status
 static int status = 0;
 // Store the last command status as string
@@ -375,339 +359,6 @@ static int __cd(int argc, char *argv[])
     return 0;
 }
 
-/// @brief Push the command inside the history.
-static inline void __hst_push(char *_cmd)
-{
-    // Reset the read index.
-    history_read_index = history_write_index;
-    // Check if it is a duplicated entry.
-    if (history_write_index > 0) {
-        if (strcmp(history[history_write_index - 1], _cmd) == 0) {
-            return;
-        }
-    }
-    // Insert the node.
-    strcpy(history[history_write_index], _cmd);
-    if (++history_write_index >= HISTORY_MAX) {
-        history_write_index = 0;
-        history_full        = true;
-    }
-    // Reset the read index.
-    history_read_index = history_write_index;
-}
-
-/// @brief Give the key allows to navigate through the history.
-static char *__hst_fetch(bool_t up)
-{
-    if ((history_write_index == 0) && (history_full == false)) {
-        return NULL;
-    }
-    // If the history is empty do nothing.
-    char *_cmd = NULL;
-    // Update the position inside the history.
-    int next_index = history_read_index + (up ? -1 : +1);
-    // Check the next index.
-    if (history_full) {
-        if (next_index < 0) {
-            next_index = HISTORY_MAX - 1;
-        } else if (next_index >= HISTORY_MAX) {
-            next_index = 0;
-        }
-        // Do no read where ne will have to write next.
-        if (next_index == history_write_index) {
-            next_index = history_read_index;
-            return NULL;
-        }
-    } else {
-        if (next_index < 0) {
-            next_index = 0;
-        } else if (next_index >= history_write_index) {
-            next_index = history_read_index;
-            return NULL;
-        }
-    }
-    history_read_index = next_index;
-    _cmd               = history[history_read_index];
-    // Return the command.
-    return _cmd;
-}
-
-/// @brief Completely delete the current command.
-static inline void __cmd_clr(void)
-{
-    // First we need to get back to the end of the line.
-    while (cmd[cmd_cursor_index] != 0) {
-        ++cmd_cursor_index;
-        puts("\033[1C");
-    }
-    memset(cmd, '\0', CMD_LEN);
-    // Then we delete all the character.
-    for (size_t it = 0; it < cmd_cursor_index; ++it) {
-        putchar('\b');
-    }
-    // Reset the index.
-    cmd_cursor_index = 0;
-}
-
-/// @brief Sets the new command.
-static inline void __cmd_set(char *_cmd)
-{
-    // Outputs the command.
-    printf(_cmd);
-    // Moves the cursore.
-    cmd_cursor_index += strlen(_cmd);
-    // Copies the command.
-    strcpy(cmd, _cmd);
-}
-
-/// @brief Erases one character from the console.
-static inline void __cmd_ers(char c)
-{
-    if ((c == '\b') && (cmd_cursor_index > 0)) {
-        strcpy(cmd + cmd_cursor_index - 1, cmd + cmd_cursor_index);
-        putchar('\b');
-        --cmd_cursor_index;
-    } else if ((c == 0x7F) && (cmd[0] != 0) && ((cmd_cursor_index + 1) < CMD_LEN)) {
-        strcpy(cmd + cmd_cursor_index, cmd + cmd_cursor_index + 1);
-        putchar(0x7F);
-    }
-}
-
-/// @brief Appends the character `c` on the command.
-static inline int __cmd_app(char c)
-{
-    if ((cmd_cursor_index + 1) < CMD_LEN) {
-        // If at the current index there is a character, shift the entire
-        // command ahead.
-        if (cmd[cmd_cursor_index] != 0) {
-            // Move forward the entire string.
-            for (unsigned long i = strlen(cmd); i > cmd_cursor_index; --i) {
-                cmd[i] = cmd[i - 1];
-            }
-        }
-        // Place the new character.
-        cmd[cmd_cursor_index++] = c;
-        return 1;
-    }
-    return 0;
-}
-
-static inline void __cmd_sug(dirent_t *suggestion, size_t starting_position)
-{
-    if (suggestion) {
-        for (size_t i = starting_position; i < strlen(suggestion->d_name); ++i) {
-            if (__cmd_app(suggestion->d_name[i])) {
-                putchar(suggestion->d_name[i]);
-            }
-        }
-        // If we suggested a directory, append a slash.
-        if (suggestion->d_type == DT_DIR) {
-            if (__cmd_app('/')) {
-                putchar('/');
-            }
-        }
-    }
-}
-
-static void __cmd_complete(void)
-{
-    // Get the lenght of the command.
-    size_t cmd_len = strlen(cmd);
-    // Count the number of words.
-    int words = __count_words(cmd);
-    // If there are no words, skip.
-    if (words == 0) {
-        return;
-    }
-    // Determines if we are at the beginning of a new argument, last character is space.
-    if (__is_separator(cmd[cmd_len - 1])) {
-        return;
-    }
-    // If the last two characters are two dots `..` append a slash `/`,
-    // and continue.
-    if ((cmd_len >= 2) && ((cmd[cmd_len - 2] == '.') && (cmd[cmd_len - 1] == '.'))) {
-        if (__cmd_app('/')) {
-            putchar('/');
-            return;
-        }
-    }
-    char cwd[PATH_MAX];
-    getcwd(cwd, PATH_MAX);
-    // Determines if we are executing a command from current directory.
-    int is_run_cmd = (words == 1) && (cmd[0] == '.') && (cmd_len > 3) && (cmd[1] == '/');
-    // Determines if we are entering an absolute path.
-    int is_abs_path = (words == 1) && (cmd[0] == '/');
-    // Prepare the dirent variable.
-    dirent_t dent;
-    // If there is only one word, we are searching for a command.
-    if (is_run_cmd) {
-        if (__folder_contains(cwd, cmd + 2, 0, &dent)) {
-            __cmd_sug(&dent, cmd_len - 2);
-        }
-    } else if (is_abs_path) {
-        char _dirname[PATH_MAX];
-        if (!dirname(cmd, _dirname, sizeof(_dirname))) {
-            return;
-        }
-        const char *_basename = basename(cmd);
-        if (!_basename) {
-            return;
-        }
-        if ((*_dirname == 0) || (*_basename == 0)) {
-            return;
-        }
-        if (__folder_contains(_dirname, _basename, 0, &dent)) {
-            __cmd_sug(&dent, strlen(_basename));
-        }
-    } else if (words == 1) {
-        if (__search_in_path(cmd, &dent)) {
-            __cmd_sug(&dent, cmd_len);
-        }
-    } else {
-        // Search the last occurrence of a space, from there on
-        // we will have the last argument.
-        char *last_argument = strrchr(cmd, ' ');
-        // We need to move ahead of one character if we found the space.
-        last_argument = last_argument ? last_argument + 1 : NULL;
-        // If there is no last argument.
-        if (last_argument == NULL) {
-            return;
-        }
-        char _dirname[PATH_MAX];
-        if (!dirname(last_argument, _dirname, sizeof(_dirname))) {
-            return;
-        }
-        const char *_basename = basename(last_argument);
-        if (!_basename) {
-            return;
-        }
-        if ((*_dirname != 0) && (*_basename != 0)) {
-            if (__folder_contains(_dirname, _basename, 0, &dent)) {
-                __cmd_sug(&dent, strlen(_basename));
-            }
-        } else if (*_basename != 0) {
-            if (__folder_contains(cwd, _basename, 0, &dent)) {
-                __cmd_sug(&dent, strlen(_basename));
-            }
-        }
-    }
-}
-
-static void __move_cursor_back(int n)
-{
-    printf("\033[%dD", n);
-    cmd_cursor_index -= n;
-}
-
-static void __move_cursor_forward(int n)
-{
-    printf("\033[%dC", n);
-    cmd_cursor_index += n;
-}
-
-/// @brief Gets the inserted command.
-static void __cmd_get(void)
-{
-    // Re-Initialize the cursor index.
-    cmd_cursor_index = 0;
-    // Initializing the current command line buffer
-    memset(cmd, '\0', CMD_LEN);
-    __set_echo(false);
-    do {
-        int c = getchar();
-        // Return Key
-        if (c == '\n') {
-            putchar('\n');
-            // Break the while loop.
-            break;
-        }
-        // It is a special character.
-        if (c == '\033') {
-            c = getchar();
-            if (c == '[') {
-                c = getchar(); // Get the char.
-                if ((c == 'A') || (c == 'B')) {
-                    char *old_cmd = __hst_fetch(c == 'A');
-                    if (old_cmd != NULL) {
-                        // Clear the current command.
-                        __cmd_clr();
-                        // Sets the command.
-                        __cmd_set(old_cmd);
-                    }
-                } else if (c == 'D') {
-                    if (cmd_cursor_index > 0) {
-                        __move_cursor_back(1);
-                    }
-                } else if (c == 'C') {
-                    if ((cmd_cursor_index + 1) < CMD_LEN && (cmd_cursor_index + 1) <= strlen(cmd)) {
-                        __move_cursor_forward(1);
-                    }
-                } else if (c == 'H') {
-                    __move_cursor_back(cmd_cursor_index);
-                } else if (c == 'F') {
-                    // Compute the offest to the end of the line, and move only if necessary.
-                    size_t offset = strlen(cmd) - cmd_cursor_index;
-                    if (offset > 0) {
-                        __move_cursor_forward(offset);
-                    }
-                } else if (c == '3') {
-                    c = getchar(); // Get the char.
-                    if (c == '~') {
-                        __cmd_ers(0x7F);
-                    }
-                }
-            }
-        } else if (c == '\b') {
-            __cmd_ers('\b');
-        } else if (c == '\t') {
-            __cmd_complete();
-        } else if (c == 127) {
-            if ((cmd_cursor_index + 1) <= strlen(cmd)) {
-                strcpy(cmd + cmd_cursor_index, cmd + cmd_cursor_index + 1);
-                putchar(127);
-            }
-        } else if (iscntrl(c)) {
-            if (c == CTRL('C')) {
-                // Re-set the index to the beginning.
-                cmd_cursor_index = 0;
-                // Go to the new line.
-                printf("\n");
-                // Sets the command.
-                __cmd_set("\0");
-                // Break the while loop.
-                break;
-            } else if (c == CTRL('U')) {
-                // Clear the current command.
-                __cmd_clr();
-                // Sets the command.
-                __cmd_set("\0");
-            } else if (c == CTRL('A')) {
-                __move_cursor_back(cmd_cursor_index);
-            } else if (c == CTRL('E')) {
-                // Compute the offest to the end of the line, and move only if necessary.
-                size_t offset = strlen(cmd) - cmd_cursor_index;
-                if (offset > 0) {
-                    __move_cursor_forward(offset);
-                }
-            } else if (c == CTRL('D')) {
-                // Go to the new line.
-                printf("\n");
-                exit(0);
-            }
-        } else if ((c > 0) && (c != '\n')) {
-            if (__cmd_app(c)) {
-                putchar(c);
-            }
-        } else {
-            pr_debug("Unrecognized character %02x (%c)\n", c, c);
-        }
-    } while (cmd_cursor_index < CMD_LEN);
-
-    // Cleans all blanks at the beginning of the command.
-    trim(cmd);
-    __set_echo(true);
-}
-
 /// @brief Gets the options from the command.
 /// @param command The executed command.
 static void __alloc_argv(char *command, int *argc, char ***argv)
@@ -819,7 +470,7 @@ static void __setup_redirects(int *argcp, char ***argvp)
     }
 }
 
-static int __execute_cmd(char *command, bool_t add_to_history)
+static int __execute_cmd(char* command)
 {
     int _status = 0;
     // Retrieve the options from the command.
@@ -831,11 +482,6 @@ static int __execute_cmd(char *command, bool_t add_to_history)
     // Check if the command is empty.
     if (_argc == 0) {
         return 0;
-    }
-
-    // Add the command to the history.
-    if (add_to_history) {
-        __hst_push(cmd);
     }
 
     if (!strcmp(_argv[0], "init")) {
@@ -900,12 +546,13 @@ static int __execute_file(char *path)
         printf("%s: %s\n", path, strerror(errno));
         return -errno;
     }
+    char cmd[BUFSIZ];
     while (fgets(cmd, sizeof(cmd), fd)) {
         if (cmd[0] == '#') {
             continue;
         }
 
-        if ((status = __execute_cmd(cmd, false)) != 0) {
+        if ((status = __execute_cmd(cmd)) != 0) {
             printf("\n%s: exited with %d\n", cmd, status);
         }
     }
@@ -923,14 +570,17 @@ static void __interactive_mode(void)
             printf("%s: .shellrc: %s\n", strerror(-ret));
         }
     }
+    // Enable the readline history
+    using_history();
 #pragma clang diagnostic push
 #pragma ide diagnostic ignored "EndlessLoop"
     while (true) {
         // First print the prompt.
         __prompt_print();
         // Get the input command.
-        __cmd_get();
-        __execute_cmd(cmd, true);
+        char *cmd = readline(NULL);
+        __execute_cmd(cmd);
+        free(cmd);
     }
 #pragma clang diagnostic pop
 }
@@ -986,7 +636,7 @@ int main(int argc, char *argv[])
                     errx(2, "%s: -c: option requires an argument", argv[0]);
                 }
 
-                if (!(status = __execute_cmd(argv[i+1], false))) {
+                if (!(status = __execute_cmd(argv[i+1]))) {
                     return status;
                 }
                 // Skip the next argument
